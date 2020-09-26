8295d3aef39c1645b81b55559ec901a4991eba70
KamranSattary <kamransattary@gmail.com>
2020-09-26 19:17:01 +0200

    Just added some extra structure to the repo and required files for submission

---
+++ src/helpers.py
@@ -0,0 +1,14 @@
+"""some helper functions."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_data(path_dataset='./data/',sub_sample=True):
+    """Load data"""
+
+    if sub_sample:
+        pass
+
+    return None
+
+
+data[1]
