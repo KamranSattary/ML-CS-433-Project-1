f71a163150a22b74a2c613fa65014425808c4f9d
KamranSattary <kamran.nejad-sattary@epfl.ch>
2020-10-19 17:24:54 +0200

    added loading data helper with possible standardization, loads train_tx, train_y, test_tx from the csv files, not sure if possible to do this directtly from the zips, i had to unzip, also not sure if standardization is always a good thing to do, does this not assume normal distribution of residuals?

--- src/helpers.py
+++ src/helpers.py
@@ -2,15 +2,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def load_data(path_dataset='./data/',sub_sample=True):
-    """Load data"""
+# didnt find how to load from zip with just numpy?
+def load_data(path_dataset='../data/', standardize = True):
 
-    if sub_sample:
-        pass
+    """
+    Loads data
+    :param path_dataset: data folder containing test.csv and train.csv
+    :return: train_y (n,), train_tx (n,d+1), test_tx (m,d+1)
+    """
 
-    return None
+    master = np.genfromtxt('../data/train.csv', delimiter=",", skip_header=1,
+                           converters={1: lambda x: float(0) if b"b" in x else float(1)})
+    train_y, train_x = master[:, 1], np.delete(master, [0, 1], axis=1)
+
+    test_x = np.genfromtxt('../data/test.csv', delimiter=",", skip_header=1)
+    test_x = np.delete(test, [0, 1], axis=1)
+
+    if standardize == True:
+        xmean, xstd = np.mean(train_tx, axis=0), np.std(train_tx, axis=0)
+        train_x = (train_x - xmean) / xstd
+        test_x = (test_x - xmean) / xstd
+
+    train_tx = np.c_[np.ones(master.shape[0]), train_x]
+    test_tx = np.c_[np.ones(test_x.shape[0]), test_x]
+
+    return train_y, train_tx, test_tx
 
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+
     """
     Generate a minibatch iterator for a dataset.
     Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
@@ -20,6 +39,7 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
     for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
         <DO-SOMETHING>
     """
+
     data_size = len(y)
 
     if shuffle:
