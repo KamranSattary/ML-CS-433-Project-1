f4a5039b3b8df4e417320e3d7f5ee51265896e90
Razvan Mocan <razvanmocan15@gmail.com>
2020-10-24 13:08:54 +0200

    First tries

--- src/run.py
+++ src/run.py
@@ -2,12 +2,18 @@ from helpers import *
 from implementations import least_squares_GD, least_squares_SGD, least_squares, \
     ridge_regression, logistic_regression, reg_logistic_regression, lasso_reg
 from proj1_helpers import *
+from nn import *
+
+from neural_net import NeuralNetwork
 
 # define constants
 DATA_TRAIN_PATH = 'data/train.csv'
+DATA_TEST_PATH = 'data/test.csv'
+
 GAMMA = 0.0001
 MAX_ITER = 100
 LAMBDA = 200
+
 
 
 def print_predition(function, params, test):
@@ -25,21 +31,38 @@ def print_predition(function, params, test):
 
 def main():
     y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
-    y = y.reshape(-1, 1)
-    test_labels, tX_test, _ = load_csv_data(DATA_TRAIN_PATH)
+    print(y.shape, tX.shape)
+    test_labels, tX_test, _ = load_csv_data(DATA_TEST_PATH)
     test_labels = test_labels.reshape(-1, 1)
     w, loss = ridge_regression(y, tX, LAMBDA)
 
     print(w)
     init_weights = np.random.random_sample((tX.shape[1], 1))
-    w, _ = lasso_reg(y, tX, init_weights, 100, GAMMA, LAMBDA)
+    w, _ = lasso_reg(y.reshape((-1, 1)), tX, init_weights, 40, GAMMA, LAMBDA)
 
     removed_features = np.where(w == 0)
     tX = np.delete(tX, removed_features, axis=1)
     tX_test = np.delete(tX_test, removed_features, axis=1)
-    init_weights = np.random.random_sample((tX.shape[1], 1))
 
-    print_predition(ridge_regression, (y, tX, LAMBDA), (test_labels, tX_test))
+    shape = len(tX[0])
+    print(tX.shape, y.shape)
+
+    NN_ARCHITECTURE = [
+        {"input_dim": shape, "output_dim": 25, "activation": "relu"},
+        {"input_dim": 25, "output_dim": 50, "activation": "relu"},
+        {"input_dim": 50, "output_dim": 50, "activation": "relu"},
+        {"input_dim": 50, "output_dim": 25, "activation": "relu"},
+        {"input_dim": 25, "output_dim": 1, "activation": "sigmoid"},
+    ]
+
+    params_values, cost_history, accuracy_history = train(tX[:10000].T, y[:10000].reshape((-1, 1)).T, NN_ARCHITECTURE, 1000, 0.01)
+    Y_test_hat, _ = full_forward_propagation(np.transpose(tX_test.T), params_values, NN_ARCHITECTURE)
+
+    print(Y_test_hat)
+    acc_test = get_accuracy_value(Y_test_hat, np.transpose(test_labels))
+    print("Test set accuracy: {:.2f} - David".format(acc_test))
+
+    # print_predition(ridge_regression, (y, tX, LAMBDA), (test_labels, tX_test))
 
 
 if __name__ == '__main__':
