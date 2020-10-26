2b970269f79a138fc00c0ab20486bd32486a9fad
KamranSattary <kamran.nejad-sattary@epfl.ch>
2020-10-26 14:23:15 +0100

    Add files via upload

--- src/run.py
+++ src/run.py
@@ -1,175 +1,112 @@
-from helpers import *
-from implementations import ridge_regression
-from proj1_helpers import *
-import argparse
-import pickle
+import numpy as np
+from helpers import build_poly, build_k_indices, fill_nan_closure, minmax_normalize_closure, standardize_closure
+from proj1_helpers import load_csv_data, predict_labels, create_csv_submission
+from implementations import ridge_regression 
 
-# define constants
-DATA_TRAIN_PATH = '../data/train.csv'
-DATA_TEST_PATH = '../data/test.csv'
-RESULT_PATH = '../data/infered.csv'
+DATA_PATH = '../data/'
+lambda_ = 1e-20
+degree = 13
+seed = 12
+k_fold = 7
 
-# define best parameters found
-DEGREE = 13
-LAMBDA = 1e-20
-K_FOLD = 7
+# We work with the training data in this notebook
+y, x, ids = load_csv_data(DATA_PATH+'train.csv')
 
-# for same k-fold
-SEED = 12
+inds = np.where(x == -999)
+x[inds] = np.nan
 
-# For jupyter notebook
-TR_ACCURACY = None
-TE_ACCURACY = None
+col_mean = np.nanmedian(x, axis=0)
+print(col_mean)
 
+#Find indices that you need to replace
+inds = np.where(np.isnan(x))
 
-def set_lambda(val):
-    """Helper function for the notebook - set LAMBDA
-    :param val: new lambda value """
-    global LAMBDA
-    LAMBDA = val
+#Place column means in the indices. Align the arrays using take
+x[inds] = np.take(col_mean, inds[1])
 
+#Minmax normalization
+xmin, xmax = np.min(x, axis=0), np.max(x, axis=0)
+x = (x - xmin) / (xmax-xmin)
 
-def save_classif_percentage(tr):
-    """This function is used to save the percentage of good classification on the test and training dataset to the
-    python notebook
+def build_poly(x, degree):
+
     """
-    global TR_ACCURACY
-
-    TR_ACCURACY = tr
-
-
-def ret_classif_percentage():
-    """This function is used to rerurn the percentage of good classification on the test and training dataset to the
-    python notebook
+    Builds polynomial augmented dataset
+    :param x: 
+    :param degree: 
+    :return:
     """
-    return TR_ACCURACY
+    r = x.copy()
+    for deg in range (2,degree+1):
+        r = np.c_[r, np.power(x, deg)]
+        
+    return np.c_[np.ones(r.shape[0]), r]
 
+def build_k_indices(y, k_fold, seed):
+    num_row = y.shape[0]
+    interval = int(num_row/k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k*interval: (k+1) * interval] for k in range (k_fold)]
+    return np.array(k_indices)
 
-def get_normalization_methods(tX):
-    """
-    Return the normalization methods initialized using training set
-    :param: tX: matrix(n, d) training set
-    :return: a list of methods that will be applied on data sets
-    """
-    # remove NaN from input data
-    fill_nan = fill_nan_closure(tX)
+def ridge_regression(y, tx, lambda_):
+    aI = 2 * tx.shape[0] * lambda_ * np.identity(tx.shape[1])
+    a = tx.T.dot(tx) + aI
+    b = tx.T.dot(y)
+    return np.linalg.solve(a,b)
 
-    # prepare minmax normalization
-    xmin, xmax = np.min(tX, axis=0), np.max(tX, axis=0)
-    minmax = minmax_normalize_closure(xmax, xmin)
+def cross_validation(y, x, k_indices, k, lambda_, degree):
+    
+    te_indice = k_indices[k]
+    tr_indice = k_indices[~(np.arange(k_indices.shape[0]) == k)]
+    tr_indice = tr_indice.reshape(-1)
+    y_te, y_tr = y[te_indice], y[tr_indice]
+    x_te, x_tr = x[te_indice], x[tr_indice]
+    
+    tx_tr = build_poly(x_tr, degree)
+    tx_te = build_poly(x_te, degree)
+    
+    w = ridge_regression(y_tr, tx_tr, lambda_)
+    
+    y_tr_pred = predict_labels(w, tx_tr)
+    y_te_pred = predict_labels(w, tx_te)
+    
+    loss_tr = sum(y_tr_pred != y_tr)/len(y_tr)
+    loss_te = sum(y_te_pred != y_te)/len(y_te)
+    
+    return loss_tr, loss_te, w
 
-    return [fill_nan, minmax]
+def cross_validation_ridge():
+   
+    k_indices = build_k_indices(y, k_fold, seed)
 
+    losses_te = []
+    losses_tr = []
+    ws = []
+     
+    for k in range(k_fold):
 
-def normalize_data(tX, methods):
-    """
-    Apply normalization methods on the dataset
-    :param tX: matrix(n, d) dataset
-    :param methods: array(k): methods to be applied
-    :return: normalized tX
-    """
-    for method in methods:
-        tX = method(tX)
-    return tX
+        loss_tr, loss_te, w = cross_validation(y, x, k_indices, k, lambda_, degree)
+        losses_te.append(loss_te)
+        losses_tr.append(loss_tr)
+        ws.append(w)
 
+    return np.mean(losses_te, axis=0), np.mean(losses_tr, axis=0), np.mean(ws, axis=0)
 
-def append_to_lists(lists, values):
-    for i in range(len(lists)):
-        lists[i].append(values[i].tolist())
+losses_te, losses_tr, w = cross_validation_ridge()
 
+print(f'Average Missclassification proportion on test folds was {losses_te}. On Train folds it was {losses_tr}.')
 
-def average_lists(lists):
-    rets = []
-    for lis in lists:
-        rets.append(np.mean(lis, axis=0))
-    return rets
+test_y, test_x, test_ids = load_csv_data(DATA_PATH + 'test.csv')
 
+# replace missing values with means determined from training data
+inds = np.where(test_x==-999)
+test_x[inds] = np.take(col_mean, inds[1])
 
-def train(tX, y):
-    """
-    Train the model
-    :param tX: model(n, d) - input
-    :param y: array(n) - labels
-    :return w: the weight of the model
-    """
-    # Split the dataset k-fold
-    k_indices = build_k_indices(len(y), K_FOLD, SEED)
+# minmax normalize again with parameters determined by train data
+test_x = (test_x - xmin) / (xmax-xmin)
 
-    ws_tmp = []
-    loss_te_tmp = []
-    loss_tr_tmp = []
-
-    for k in range(K_FOLD):
-        training, testing = cross_validation(y, tX, k_indices, k)
-
-        # build the polynomial fct
-        tx_tr = build_poly(training[0], DEGREE)
-        tx_te = build_poly(testing[0], DEGREE)
-
-        # Train the model
-        w, _ = ridge_regression(training[1], tx_tr, LAMBDA)
-
-        # Compute loss
-        pred_tr = predict(tx_tr, w)
-        pred_te = predict(tx_te, w)
-
-        loss_tr = np.sum(np.equal(pred_tr, training[1])) / len(pred_tr)
-        loss_te = np.sum(np.equal(pred_te, testing[1])) / len(pred_te)
-
-        append_to_lists((ws_tmp, loss_tr_tmp, loss_te_tmp), (w, loss_tr, loss_te))
-
-    w, good_tr, good_te = average_lists((ws_tmp, loss_tr_tmp, loss_te_tmp))
-
-    save_classif_percentage(good_tr)
-    print("Training set good classification {}".format(good_tr))
-
-    return w
-
-
-def save_predict(tX, w, inds):
-    """
-    Save predictions
-    :param tX: matrix(n, d) data to be predicted
-    :param w: array(d) model weights
-    :param inds;
-    "return None
-    """
-    pred = predict(build_poly(tX, DEGREE), w)
-
-    create_csv_submission(inds, pred, args.result_path)
-
-
-def main():
-    # Get training set and compute normalization params
-    y, tX, ids = load_csv_data(args.train_path)
-    norm_method = get_normalization_methods(tX)
-
-    if args.w is None:
-        # No saved weights => normalize and train
-        tX = normalize_data(tX, norm_method)
-        w = train(tX, y)
-    else:
-        # Otherwise load the saved weight
-        with open(args.w, 'rb') as f:
-            w = pickle.load(f)
-
-    # Get an normalized pred set + run inference and save
-    _, tX_pred, ids_pred = load_csv_data(args.infer_path)
-    tX_pred = normalize_data(tX_pred, norm_method)
-    save_predict(tX_pred, w, ids_pred)
-
-
-if __name__ == '__main__':
-    # command line arguments
-    parser = argparse.ArgumentParser(description='Train(optional) model and generate prediction')
-    parser.add_argument('-train_path', default=DATA_TRAIN_PATH, help='Train dataset full path; otherwise assumed')
-    parser.add_argument('-infer_path', default=DATA_TEST_PATH, help='Predict dataset full path; otherwise assumed')
-    parser.add_argument('-result_path', default=RESULT_PATH, help='Where to save the inference result full path; '
-                                                                  'otherwise assumed')
-    parser.add_argument('-w', default=None,
-                        help='Path to pickle file of weights. If present just the predict step will '
-                             'take place')
-
-    args = parser.parse_args()
-
-    main()
+# create final predictions on testing data and submission csv
+y_pred = predict_labels(w, build_poly(test_x, degree))
+create_csv_submission(test_ids, y_pred, DATA_FOLDER+'inferred.csv')
